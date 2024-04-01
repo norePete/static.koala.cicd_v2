037fb896898afc194939e9db696517d8dbf942c7
PETER HOOPER <peter_hooper@hotmail.co.nz>
2024-04-01 18:45:20 +1300

    run

---
+++ .development/src/libs/utils.js
@@ -0,0 +1,9 @@
+function suid(){
+    let date = (Date.now()).toString();
+    const sub = date.substring(date.length-6,date.length-1);
+    return sub+"_"+Math.floor(Math.random() * 10000)
+}
+
+export {
+    suid
+}
--- .development/tailwind.config.cjs
+++ .development/tailwind.config.cjs
@@ -2,10 +2,20 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-        'regal-blue': '#243c5a',
-        transitionDuration: {
-        '2000': '2000ms',
-          },
+            'regal-blue': '#243c5a',
+            transitionDuration: { '2000': '2000ms', },
+            width: { '400': '1000px', },
+            minWidth: { '720': '720px', },
+            height: { '400': '1000px', },
+            keyframes: {
+                banner: {
+                  '0%':  { opacity: '0' },
+                  '15%': { opacity: '0' },
+                  '50%': { opacity: '1' },
+                  '85%': { opacity: '0' },
+                  '100%': { transform: 'translateX(100vw)' },
+                },
+            },
         },
 	},
 	plugins: [],
